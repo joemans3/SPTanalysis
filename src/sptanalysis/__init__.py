619da070610a804bc961797828f09c1596474a8a
Baljyot Parmar <joemans3@users.noreply.github.com>
2025-02-11 04:32:17 -0500

    feat: made src/ project structure

---
+++ src/sptanalysis/__init__.py
@@ -0,0 +1,12 @@
+"""
+Single Particle Tracking Analysis code.
+
+Main functionality for MSD and angle calculations. Also includes nearest neigbour displacement calculations.
+GitHub: https://github.com/joemans3/SPTAnalysis
+Pypi: https://pypi.org/project/SPTAnalysis/
+    - pip install SPTAnalysis
+Author: Baljyot Singh Parmar
+Last updated: 2025-02-11
+"""
+
+__version__ = "0.0.1"
